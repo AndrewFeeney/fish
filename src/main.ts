15e7de6d20b480be5fe1588e32670bb9f848c561
Andrew Feeney <andrew@deskspacecreative.com.au>
2023-01-06 20:38:23 +1100

    Display score

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import Game from './game'
 import GameClock from './game-clock'
 import { Dimensions2D } from './dimensions-2d'
 import { newGameConfig } from './game-config'
-import { gameEventBus } from './events'
+import { Event, gameEventBus } from './events'
+import Player from './player'
 
 const boardDimensions: Dimensions2D = {
   width: 1080,
@@ -23,6 +24,12 @@ const ctx = canvas.getContext('2d') as CanvasRenderingContext2D
 const gameConfig = newGameConfig()
 const clock = new GameClock()
 const eventBus = gameEventBus()
+eventBus.on(Event.PlayerScoreUpdated, (payload: { player: Player, score: number }) => {
+  const scoreElement = document.getElementById('score')
+  if (scoreElement) {
+    scoreElement.innerHTML = payload.score.toString()
+  }
+})
 const game = new Game(ctx, gameConfig, clock, eventBus)
 
 game.start()
